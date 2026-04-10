e3bcf7f2632820e3f98a10de6277d86ec3afbc0d
Holman Gonzalez Andrade <87282332+HolmanDavidG@users.noreply.github.com>
2026-04-10 16:21:16 -0600

    update test

--- src/components/WeatherCard.test.js
+++ src/components/WeatherCard.test.js
@@ -1,3 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import WeatherCard from './WeatherCard';
+
+test('renders the weather card heading', () => {
+    render(<WeatherCard />);
+    const headingElement = screen.getByText(/Edmonton Weather/i);
+    expect(headingElement).toBeInTheDocument();
+});
